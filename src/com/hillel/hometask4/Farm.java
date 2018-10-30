27ce6038b891ad59b6d074e1e9a62ea13a76a2fa
al3221 <yaroshenkoalex100@gmail.com>
2018-10-30 19:31:55 +0200

    New task

--- src/com/hillel/hometask4/Farm.java
+++ src/com/hillel/hometask4/Farm.java
@@ -2,13 +2,27 @@ package com.hillel.hometask4;
 
 public class Farm {
     public static int perimeter(int l, int w){
-        int p = (l+w)*2;
-        int result = p;
-        return p;
+        if(l<=0 || w<=0){
+            System.out.println("Ошибка");
+            System.exit(0);
+        }
+        else {
+            int p = (l + w) * 2;
+            int result = p;
+            return p;
+        }
+        return 0;
     }
     public static int square(int l, int w){
-        int p = l*w;
-        int result = p;
-        return p;
+        if(l<=0 || w<=0){
+            System.out.println("Ошибка");
+            System.exit(0);
+        }
+        else {
+            int p = l * w;
+            int result = p;
+            return p;
+        }
+        return 0;
     }
 }
