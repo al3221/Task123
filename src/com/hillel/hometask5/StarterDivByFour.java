ad7b3e39f4c112c7c4eb498f0291844f2af14784
al3221 <yaroshenkoalex100@gmail.com>
2018-11-02 22:22:36 +0200

    New task

--- src/com/hillel/hometask5/StarterDivByFour.java
+++ src/com/hillel/hometask5/StarterDivByFour.java
@@ -2,7 +2,7 @@ package com.hillel.hometask5;
 
 public class StarterDivByFour {
     public static void main(String[] args){
-        long result = DivByFour.bitDivByFour(1000000000.0);
+        long result = DivByFour.bitDivByFour(1000000001.0);
         System.out.println(result);
     }
 }
