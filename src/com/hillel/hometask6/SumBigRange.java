58589124231fa0cf09ec6f4af63304c7d8c38ef9
al3221 <yaroshenkoalex100@gmail.com>
2018-11-07 14:03:25 +0200

    New task

--- src/com/hillel/hometask6/SumBigRange.java
+++ src/com/hillel/hometask6/SumBigRange.java
@@ -5,7 +5,7 @@ import com.hillel.utilities.Random;
 public class SumBigRange {
     public static void bigRange(){
         int sum = 0;
-        for(int i =0;i<=100;i++){
+        for(int i =0;i<100;i++){
             int n = Random.randomizer(-20,20);
             sum = sum + n;
             System.out.println(n);
--- src/com/hillel/hometask6/SumSmallRange.java
+++ src/com/hillel/hometask6/SumSmallRange.java
@@ -5,7 +5,7 @@ import com.hillel.utilities.Random;
 public class SumSmallRange {
     public static void smallRange(){
         int sum = 0;
-        for(int i =0;i<=100;i++){
+        for(int i =0;i<100;i++){
             int n = Random.randomizer(-10,9);
             sum = sum + n;
             System.out.println(n);
