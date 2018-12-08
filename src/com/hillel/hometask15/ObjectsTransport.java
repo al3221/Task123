baea6872070a7f4aa745f9df540b15240b74dfdd
al3221 <yaroshenkoalex100@gmail.com>
2018-12-08 20:55:28 +0200

    New task

--- src/com/hillel/hometask15/ObjectsTransport.java
+++ src/com/hillel/hometask15/ObjectsTransport.java
@@ -2,14 +2,14 @@ package com.hillel.hometask15;
 
 public class ObjectsTransport {
     public static void main(String[] args) {
-        Train train = new Train("Donbass DNR150" ,150,0,50);
+        Train train = new Train("KirovZhD-13" ,150,0,50);
         train.model();
         train.nowTmpSpeedOfTrain();
         train.numOfPassengersInTrain(5);
 
         System.out.println();
 
-        Plane plane = new Plane("RussianWorld C2H5OH",1500,0,250);
+        Plane plane = new Plane("MIG-25",1500,0,250);
         plane.model();
         plane.nowTmpSpeedOfPlane();
         plane.showColour("Blue");
